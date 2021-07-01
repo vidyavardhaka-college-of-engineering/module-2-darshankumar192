aa8e128226906f77dc5ccfd028c0bb93aac92cca
Darshan Kumar B M <84765811+darshankumar192@users.noreply.github.com>
2021-07-01 14:27:39 +0530

    Update Program-6.c

--- Program-6.c
+++ Program-6.c
@@ -16,8 +16,8 @@ int main()
  }
  evennum = 0; // Assuming 0 even numbers
  oddnum = 0; // Assuming 0 odd numbers
- for(i=0; i<num; i++)
-{
+ for(i=0; i<num; i++) 
+ {
  /* If the current element of array is evennumber then increment evennumber count */
  if(arr[i]%2 == 0) 
  {
@@ -31,5 +31,5 @@ int main()
 printf("Total number of even numbers and odd numbers in the array are %d and 
 %d\n",evennum,oddnum);
 //printf("Total number of odd numbers in the array : %d\n", );
-return(0); 
+return 0; 
 }
